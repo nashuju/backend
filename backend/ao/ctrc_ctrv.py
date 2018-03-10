3bb069e3c70ee068a214c91e61626db16efc2aa5
micheal <434835764@qq.com>
2018-03-10 16:41:32 +0800

    modify ctrc_ctrv

--- backend/ao/ctrc_ctrv.py
+++ backend/ao/ctrc_ctrv.py
@@ -1,11 +1,19 @@
 import pyperclip
 import time
-
+from urllib import parse , request
 
 tmp = ""
+if pyperclip.paste().__eq__('__break__'):pyperclip.copy("__restart__")
+user_email = "434835764@qq.com"
 while True:
     if tmp.__eq__('__break__') or tmp.__eq__("__break__"): break;
     if tmp != pyperclip.paste():
         print (pyperclip.paste())
         tmp = pyperclip.paste()
+        url_encode = parse.quote(tmp)
+        try:
+            url = "http://www.nashuju.com:8082/index/visit/"+user_email+"/"+ url_encode
+            response = request.urlopen(url)
+        except:
+            continue
         time.sleep(1)
