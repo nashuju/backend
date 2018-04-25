4fdf127527b572735ed88d32fcc74f0ab5fdba62
micheal <434835764@qq.com>
2018-04-25 14:16:58 +0800

    fix fanyi

--- backend/ao/baiduTranslateApi.py
+++ backend/ao/baiduTranslateApi.py
@@ -74,19 +74,12 @@ def FR2ZH(context):
 def all2ZH(context):
     pattern = re.compile('[a-z，A-Z]+')
     match = pattern.findall(context)
-    c = langid.classify(match[0])[0]
     if match.__len__() != 1 : return context,'unknown2zh'
-    if c == 'en':
-        ret = EN2ZH(match[0])
-        if langid.classify(ret)[0] == 'zh':
-            return ret,'en2zh'
-        else:
-            return context,'unknown2zh'
-    elif c == 'fr':
-        ret = FR2ZH(match[0])
-        if langid.classify(ret)[0] == 'zh':
-            return ret,'fr2zh'
-        else:
-            return context,'unknown2zh'
+    ret = EN2ZH(match[0])
+    if langid.classify(ret)[0] == 'zh':
+        return ret,'en2zh'
     else:
         return context,'unknown2zh'
+
+
+# print EN2ZH('liberalise')
